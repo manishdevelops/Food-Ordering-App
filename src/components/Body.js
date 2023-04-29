2d4a174d6907a0f84384bcb21c087cd5b4fcbbbf
manish mandal <76776194mm@gmail.com>
2023-04-29 22:48:55 +0530

    add filter data

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,50 @@
 import { restrauntList } from './Config';
 import RestrauntCard from './RestrauntCard';
+import { useState } from 'react';
 
-// console.log(restrauntList);
+function filterData(searchText, restraunts) {
+	console.log(searchText);
+	console.log(restraunts);
+
+	const filterData = restraunts.filter((restraunt) => {
+		console.log(restraunt.data.name);
+		return restraunt.data.name.includes(searchText);
+	});
+	console.log(filterData);
+	return filterData;
+}
 const Body = () => {
+	const [restraunts, setRestraunts] = useState(restrauntList);
+	const [searchText, setSearchText] = useState('');
+
 	return (
-		<div className='restraunt-list'>
-			{restrauntList.map((restraunt) => (
-				<RestrauntCard {...restraunt.data} key={restraunt.data.id} />
-			))}
-		</div>
+		<>
+			<div className='search-container'>
+				<input
+					type='text'
+					className='search-input'
+					placeholder='Search'
+					value={searchText}
+					onChange={(e) => setSearchText(e.target.value)}
+				/>
+				<button
+					className='search-btn'
+					onClick={() => {
+						const data = filterData(searchText, restraunts);
+						console.log(data);
+						console.log(searchText);
+						setRestraunts(data);
+					}}
+				>
+					Search
+				</button>
+			</div>
+			<div className='restraunt-list'>
+				{restraunts.map((restraunt) => (
+					<RestrauntCard {...restraunt.data} key={restraunt.data.id} />
+				))}
+			</div>
+		</>
 	);
 };
 
