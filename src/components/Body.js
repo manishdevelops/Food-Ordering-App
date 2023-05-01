30a840f9e3b6c68d3b02891230a9e6bc8c5b8c38
manish mandal <76776194mm@gmail.com>
2023-05-01 23:49:52 +0530

    modified

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,23 +1,47 @@
-import { restrauntList } from './Config';
+// import { restrauntList } from './Config';
 import RestrauntCard from './RestrauntCard';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import Shimmer from './Shimmer.js';
 
 function filterData(searchText, restraunts) {
-	console.log(searchText);
-	console.log(restraunts);
-
 	const filterData = restraunts.filter((restraunt) => {
-		console.log(restraunt.data.name);
 		return restraunt.data.name.includes(searchText);
 	});
-	console.log(filterData);
+	// console.log(filterData);
 	return filterData;
 }
 const Body = () => {
-	const [restraunts, setRestraunts] = useState(restrauntList);
+	const [allRestraunts, setAllRestraunts] = useState([]);
+	const [filteredRestraunts, setFilteredRestraunts] = useState([]);
 	const [searchText, setSearchText] = useState('');
 
-	return (
+	useEffect(() => {
+		getRestraunts();
+	}, []);
+	console.log('render');
+
+	async function getRestraunts() {
+		try {
+			const data = await fetch(
+				'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING'
+			);
+			console.log(data);
+			if (!data.ok) {
+				throw Error('wrong url');
+			}
+			const json = await data.json();
+			console.log(json);
+			setAllRestraunts(json?.data?.cards[2]?.data?.data?.cards);
+			setFilteredRestraunts(json?.data?.cards[2].data?.data?.cards);
+			// console.log('render');
+		} catch (err) {
+			console.log(err);
+			throw new Error(`something went wrong ${err.message}`);
+		}
+	}
+	return allRestraunts.length === 0 ? (
+		<Shimmer />
+	) : (
 		<>
 			<div className='search-container'>
 				<input
@@ -30,17 +54,17 @@ const Body = () => {
 				<button
 					className='search-btn'
 					onClick={() => {
-						const data = filterData(searchText, restraunts);
-						console.log(data);
-						console.log(searchText);
-						setRestraunts(data);
+						const data = filterData(searchText, allRestraunts);
+						// console.log(data);
+						// console.log(searchText);
+						setFilteredRestraunts(data);
 					}}
 				>
 					Search
 				</button>
 			</div>
 			<div className='restraunt-list'>
-				{restraunts.map((restraunt) => (
+				{filteredRestraunts.map((restraunt) => (
 					<RestrauntCard {...restraunt.data} key={restraunt.data.id} />
 				))}
 			</div>
