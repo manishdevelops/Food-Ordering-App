7d9e7e524f8b5b8b949b25b5ece8ffc3fdb9be77
manish mandal <76776194mm@gmail.com>
2023-05-03 23:09:06 +0530

    add login/logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,10 @@
+import { useState } from 'react';
+
+const loggedInUser = () => {
+	// API call to check authentication
+	return true;
+};
+
 const Title = () => (
 	<a href='/'>
 		<img
@@ -9,6 +16,8 @@ const Title = () => (
 );
 
 const Header = () => {
+	const [isLoggedIn, setLoggedIn] = useState(true);
+
 	return (
 		<div className='header'>
 			<Title />
@@ -20,6 +29,11 @@ const Header = () => {
 					<li>Cart</li>
 				</ul>
 			</div>
+			{isLoggedIn ? (
+				<button onClick={() => setLoggedIn(false)}>Login</button>
+			) : (
+				<button onClick={() => setLoggedIn(true)}>Logout</button>
+			)}
 		</div>
 	);
 };
