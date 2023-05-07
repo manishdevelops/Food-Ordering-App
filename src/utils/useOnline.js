601a21c86a256d97dfbba2f574c6210e3038c811
manish mandal <76776194mm@gmail.com>
2023-05-07 15:53:52 +0530

    add online status

---
+++ src/utils/useOnline.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+
+const useOnline = () => {
+	const [isOnline, setOnline] = useState(true);
+	useEffect(() => {
+		window.addEventListener('online', () => {
+			setOnline(true);
+		});
+		window.addEventListener('offline', () => {
+			setOnline(false);
+		});
+	}, []);
+
+	return isOnline;
+};
+
+export default useOnline;
