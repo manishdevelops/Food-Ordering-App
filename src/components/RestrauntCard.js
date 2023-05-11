ef94283dd985dcf93e6a0224f78846085a227745
manish mandal <76776194mm@gmail.com>
2023-05-11 23:58:42 +0530

    modified

--- src/components/RestrauntCard.js
+++ src/components/RestrauntCard.js
@@ -1,4 +1,6 @@
 import { IMG_CDN_URL } from '../Config';
+import { useContext } from 'react';
+import UserContext from '../utils/UserContext';
 
 const RestrauntCard = ({
 	name,
@@ -7,12 +9,15 @@ const RestrauntCard = ({
 	lastMileTravelString,
 }) => {
 	// console.log(name, cuisines, cloudinaryImageId, lastMileTravelString);
+	const { user, email } = useContext(UserContext);
 	return (
 		<div className='w-[200px] p-2 m-2 shadow-lg'>
 			<img src={IMG_CDN_URL + cloudinaryImageId} alt='burder' />
 			<h2 className='font-bold text-2xl'>{name}</h2>
 			<h3>{cuisines.join(', ')}</h3>
-			<h4>{lastMileTravelString} minutes</h4>
+			<h4>{lastMileTravelString}</h4>
+			{/* <h5 className='font-bold'>{user.name}</h5>
+			<h5 className='font-bold'>{user.email}</h5> */}
 		</div>
 	);
 };
