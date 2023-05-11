157c51b5a5fe2c2a3d2e733c3e20967197d31fc3
manish mandal <76776194mm@gmail.com>
2023-05-11 23:57:00 +0530

    modified

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,14 @@
+import { Outlet } from 'react-router-dom';
+
 const About = () => {
 	return (
 		<div>
 			<h1>About Us Page</h1>
 			<p>
-				{' '}
-				This is the Namaste React Live Course Chapter 07 - Finding the Path 🫡
+				This is the Namaste React Live Course Chapter 07 - Finding the Path 🫡.
 			</p>
+
+			<Outlet />
 		</div>
 	);
 };
