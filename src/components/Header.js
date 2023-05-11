852d8185643aa1eae4bb0d72c156dc271d131790
manish mandal <76776194mm@gmail.com>
2023-05-11 23:57:42 +0530

    modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,44 @@
-import { useState } from 'react';
-
-const loggedInUser = () => {
-	// API call to check authentication
-	return true;
-};
+import { useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import useOnline from '../utils/useOnline';
+import { logo } from '../Config.js';
+import UserContext from '../utils/UserContext';
 
 const Title = () => (
 	<a href='/'>
-		<img
-			className='logo'
-			alt='logo'
-			src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBL9bhJXk8totURVHPPIDg2cMdLJxEqkAaqR2YDRgwx1Bk0F5ihRvqSWy_S-C2YDnj2j8&usqp=CAU'
-		/>
+		<img className='h-28 p-2' alt='logo' src={logo} />
 	</a>
 );
 
 const Header = () => {
 	const [isLoggedIn, setLoggedIn] = useState(true);
+	const isOnline = useOnline();
+	const { user } = useContext(UserContext);
 
 	return (
-		<div className='header'>
+		<div className='flex justify-between bg-pink-50 shadow-lg sm:bg-blue-50'>
 			<Title />
 			<div className='nav-items'>
-				<ul>
-					<li>Home</li>
-					<li>About</li>
-					<li>Contact</li>
-					<li>Cart</li>
+				<ul className='flex py-10'>
+					<li className='px-2'>
+						<Link to='/'>Home</Link>
+					</li>
+					<li className='px-2'>
+						<Link to='/about'>About</Link>
+					</li>
+					<li className='px-2'>
+						<Link to='/contact'>Contact</Link>
+					</li>
+					<li className='px-2'>
+						<Link to='/cart'>Cart</Link>
+					</li>
+					<li className='px-2'>
+						<Link to='/instamart '>Instamart</Link>
+					</li>
 				</ul>
 			</div>
+			<h1>{isOnline ? '✅' : '❌'}</h1>
+			<span className='p-10 font-bold text-red-900'>{user.name}</span>
 			{isLoggedIn ? (
 				<button onClick={() => setLoggedIn(false)}>Login</button>
 			) : (
